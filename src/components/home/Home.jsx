c65019fab3d8b37d9f270a7a5f04563e664b269b
Linda.Mejia <linda.mejia@uconn.edu>
2019-06-05 19:25:19 -0700

    fixing api

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -10,7 +10,7 @@ export default class Home extends Component {
       }
     };
     this.END_POINT =
-      "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+      "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
   }
 
   componentDidMount() {
